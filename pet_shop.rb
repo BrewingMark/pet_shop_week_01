d32fcdd383dff5d96d30334186652fcf7bae24cc
Mark Nicol <marknicol1991@live.co.uk>
2020-03-13 14:30:52 +0000

    adds functions to + or - total cash,and to return number of pets sold

--- pet_shop.rb
+++ pet_shop.rb
@@ -5,3 +5,11 @@ end
 def total_cash(pet_shop)
   return pet_shop[:admin][:total_cash]
 end
+
+def add_or_remove_cash(pet_shop, cash_difference)
+  pet_shop[:admin][:total_cash] = pet_shop[:admin][:total_cash] + cash_difference
+end
+
+def pets_sold(pet_shop)
+  return pet_shop[:admin][:pets_sold]
+end
