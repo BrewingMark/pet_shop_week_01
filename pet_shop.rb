79912af62ada31847338b952db1beb3e4feef59f
Mark Nicol <marknicol1991@live.co.uk>
2020-03-15 13:04:20 +0000

    Adds a fuction to check if a customer can afford a particular pet.

--- pet_shop.rb
+++ pet_shop.rb
@@ -66,3 +66,11 @@ end
 def add_pet_to_customer(customer, pet)
   customer[:pets] << pet
 end
+
+def customer_can_afford_pet(customer, pet)
+  if customer[:cash] >= pet[:price]
+    return true
+  else
+    return false
+  end
+end
